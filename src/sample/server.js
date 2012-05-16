ace561fef2fd7c9f4de712005bb179ce195d1017
Tomasz Janczuk <tomasz@janczuk.org>
2012-05-16 10:00:22 -0700

    remove dependency on node-static

--- src/sample/server.js
+++ src/sample/server.js
@@ -1,10 +1,24 @@
-var static = require('node-static')
+var fs = require('fs')
+	, path = require('path')
 	, port = 31415;
 
-var file = new(static.Server)('./');
+var routes = {
+	'/checkout.html': fs.readFileSync(path.resolve(__dirname, 'checkout.html')),
+	'/scripts/phone2web.js': fs.readFileSync(path.resolve(__dirname, 'scripts/phone2web.js'))
+};
 
-require('http').createServer(function (request, response) {
-  request.addListener('end', function () {
-    file.serve(request, response);
-  });
+var contentTypes = {
+	'/checkout.html': 'text/html',
+	'/scripts/phone2web.js': 'application/javascript'
+};
+
+require('http').createServer(function (req, res) {
+	if (req.method === 'GET' && routes[req.url]) {
+		res.writeHead(200, { 'Content-Type': contentTypes[req.url]});
+		res.end(routes[req.url]);
+	}
+	else {
+		res.writeHead(404);
+		res.end();
+	}
 }).listen(process.env.PORT || port);
