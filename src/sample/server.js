7f6ab142b5966b8f5d116f03f4cbb9c514d15366
Tomasz Janczuk <tomasz@janczuk.org>
2012-05-16 10:03:53 -0700

    refactor routes

--- src/sample/server.js
+++ src/sample/server.js
@@ -3,19 +3,20 @@ var fs = require('fs')
 	, port = 31415;
 
 var routes = {
-	'/checkout.html': fs.readFileSync(path.resolve(__dirname, 'checkout.html')),
-	'/scripts/phone2web.js': fs.readFileSync(path.resolve(__dirname, 'scripts/phone2web.js'))
-};
-
-var contentTypes = {
-	'/checkout.html': 'text/html',
-	'/scripts/phone2web.js': 'application/javascript'
+	'/checkout.html': {
+		body: fs.readFileSync(path.resolve(__dirname, 'checkout.html')),
+		contentType: 'text/html'
+	},
+	'/scripts/phone2web.js': {
+		body: fs.readFileSync(path.resolve(__dirname, 'scripts/phone2web.js')),
+		contentType: 'application/javascript'
+	}
 };
 
 require('http').createServer(function (req, res) {
 	if (req.method === 'GET' && routes[req.url]) {
-		res.writeHead(200, { 'Content-Type': contentTypes[req.url]});
-		res.end(routes[req.url]);
+		res.writeHead(200, { 'Content-Type': routes[req.url].contentType});
+		res.end(routes[req.url].body);
 	}
 	else {
 		res.writeHead(404);
